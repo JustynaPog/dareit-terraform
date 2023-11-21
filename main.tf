4612060869651af6fddcd1b51e901bfd988e3811
JustynaPog <147341351+JustynaPog@users.noreply.github.com>
2023-11-21 21:12:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,4 @@ resource "google_storage_bucket" "terra_pip_bucket" {
   name          = "dareit-pipeline123-bucket"
   location      = "US"  
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-
-  uniform_bucket_level_access = false
 }
